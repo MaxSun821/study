2053ca39f461b030034b5a3bd1437ef095967538
maxsun821 <max.y.h.sun@outlook.com>
2023-05-13 15:20:14 +0800

    static修饰全局变量、函数、局部变量

--- day1/static.h
+++ day1/static.h
@@ -1,10 +1,11 @@
 //
-// Created by 孙宇涵 on 2023/5/13.
+// Created by Max on 2023/5/13.
 //
 
 #ifndef DAY1_STATIC_H
 #define DAY1_STATIC_H
 
+#pragma once
 #include <stdio.h>
 extern int g_val;
 extern void func();
