b68f7092ad2ce94e005d6bfdd4da613898ff20e9
maxsun821 <max.y.h.sun@outlook.com>
2023-05-27 18:47:42 +0800

    符号

--- day3/sign.c
+++ day3/sign.c
@@ -5,8 +5,26 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <math.h>
 
 int main() {
+    printf("%d\n", -10 / 3);
+    printf("%d\n", -10 % 3);
+
+//    double i = 2.9;
+//    double j = -2.9;
+//    printf("%f\n", trunc(i));
+//    printf("%f\n", trunc(j));
+
+//    int i = 1;
+//    int j = (++i) + (++i) + (++i);
+//    printf("%d\n", j);
+//    char c = 'a';
+//    printf("sizeof(c): %d\n", sizeof(c));
+//    printf("sizeof(~c): %d\n", sizeof(~c));
+//    printf("sizeof(c<<1): %d\n", sizeof(c<<1));
+//    printf("sizeof(c>>1): %d\n", sizeof(c>>1));
+
     // int i = 0;
     // for(i = 9; i >= 0; i--) {
     //     sleep(1);
@@ -15,14 +33,14 @@ int main() {
     // }
 
 
-   int index = 0;
-   const char* lable = "|/-\\";
-   while(1) {
-       index %= 4;
-       printf("[%c]\r", lable[index]);
-       index++;
-       sleep(0.5);
-   }
+//   int index = 0;
+//   const char* lable = "|/-\\";
+//   while(1) {
+//       index %= 4;
+//       printf("[%c]\r", lable[index]);
+//       index++;
+//       sleep(0.5);
+//   }
 
 
     return 0;
